9ba8a8eaf955bea602324b561a9c39c920e5ea90
Kirkjaytablada-UCM <kirkjaytablada@gmail.com>
2025-08-29 23:06:47 +0800

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,15 @@
+public class Comments {
+    public static void main(String[] args) {
+        // This is a single-line comment
+
+        /*
+         * This is a multi-line comment.
+         * It can span multiple lines.
+         */
+
+        /**
+         * This is a documentation comment.
+         * It is used to generate documentation for the code.
+         */
+    }
+}
---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("I fell right through the cracks");
+        System.out.println("Now I'm trying to get back");
+        System.out.println("Before the cool done run out");
+        System.out.println("I'll be giving it my bestest");
+    }
+}
