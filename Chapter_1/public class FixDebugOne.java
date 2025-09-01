bfb7501b6db2a7730b36995609ebfd5939f091a5
Kirkjaytablada-UCM <kirkjaytablada@gmail.com>
2025-09-01 23:13:54 +0800

    Add files via upload

---
+++ Chapter_1/public class FixDebugOne.java
@@ -0,0 +1,15 @@
+public class FixDebugOne {
+    /* This program displays a greeting */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+/*
+Bugs spotted and fixed:
+1. Class name had a space "FixDebug One" it must be "FixDebugOne"
+2. The word "Main" is Capital. It should be lowercase "main"
+3. The parameter is wrong  "String args" it should be "String[] args"
+4. The "System" was misspelled "Systm"
+5. Missing semicolon at the end of println statement
+*/
