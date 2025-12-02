a09854017735b27f72888a3fc5be2017e0134d78
Kirkjaytablada-UCM <kirkjaytablada@gmail.com>
2025-12-02 19:53:49 +0800

    Add files via upload

---
+++ Chapter_8/DebugEight1.java
@@ -0,0 +1,40 @@
+// Application lists valid shipping codes
+// A, C, T, or H
+// then prompts user for a code
+// Application accepts a shipping code
+// and determines if it is valid
+import java.util.*;
+public class DebugEight1
+{
+public static void main(String args[])
+{
+Scanner input = new Scanner(System.in);
+char userCode;
+String entry;
+boolean found = true;
+char[] okayCodes = {'A','C','T','H'};
+System.out.println("Enter shipping code for this delivery.");
+System.out.print("Valid codes are: ");
+for(int x = 0; x < 2; ++x)
+{
+System.out.print(okayCodes[x]);
+if(x != (okayCodes.length - 1))
+System.out.print(", ");
+}
+System.out.print(" >> ");
+entry = input.nextLine();
+userCode = entry.charAt(1);
+for(int i = 0; i < okayCodes.length; ++i)
+{
+if(userCode == okayCodes[i]) // its supposed to be 2 (=) so i added 1
+{
+found = true;
+break; //forgot to put break
+}
+}
+if(!found)
+System.out.println("Good code");
+else
+System.out.println("Sorry code not found");
+}
+}
